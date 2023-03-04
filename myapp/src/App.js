6ecf74dcce4078ba3d0f337eb25e8bc2c0a96242
vaibhavsixt <vaibhav.singh@sixt.com>
2023-03-04 13:50:25 +0530

    food menu

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,15 +5,17 @@ import ThemeProvider from './theme';
 // components
 import ScrollToTop from './components/scroll-to-top';
 import { StyledChart } from './components/chart';
+import FoodManager from './containers/FoodManager'
 
 // ----------------------------------------------------------------------
 
 export default function App() {
   return (
     <ThemeProvider>
-      <ScrollToTop />
+      <FoodManager />
+      {/* <ScrollToTop />
       <StyledChart />
-      <Router />
+      <Router /> */}
     </ThemeProvider>
   );
 }
